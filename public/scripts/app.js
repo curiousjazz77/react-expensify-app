7c74448fcf197cbec5bf8ca48b1fa523ca173ea1
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-07 21:40:26 -0500

    fix formatting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,20 +4,18 @@ console.log('App.js is running!');
 
 //JSX - Javascript XML extension provided to us by react, not native to javascript
 var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
+    'div',
     null,
-    'Indecision App'
-  ),
-  ' ',
-  React.createElement(
-    'p',
-    null,
-    'This is some info'
-  ),
-  ' '
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    )
 );
 var appRoot = document.getElementById('app');
 

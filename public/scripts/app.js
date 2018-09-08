231b262b4462f17e1d47fa8deb749cd5408e0d03
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-07 21:38:23 -0500

    multitagging description

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,9 +4,20 @@ console.log('App.js is running!');
 
 //JSX - Javascript XML extension provided to us by react, not native to javascript
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    'Indecision App'
+  ),
+  ' ',
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  ),
+  ' '
 );
 var appRoot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 console.log('App.js is running!');
 
 //JSX - Javascript XML extension provided to us by react, not native to javascript
-var template = <h1>Indecision App</h1>;
+var template = <div><h1>Indecision App</h1> <p>This is some info</p> </div>;
 var appRoot = document.getElementById('app')
 
 ReactDOM.render(template, appRoot);

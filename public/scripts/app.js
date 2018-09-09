1a454671a01a84311d78fa426c870044955afbac
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-09 02:39:44 -0500

    render update to options list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,53 +5,73 @@ console.log('App.js is running!');
 var app = {
     title: 'Indecision app',
     subtitle: 'Put your choices in the hands of a computer',
-    options: ['One', 'Two']
+    options: []
 };
 
-//JSX - Javascript XML extension provided to us by react, not native to javascript
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title ? app.title + '!' : 'Null'
-    ),
-    app.subtitle ? React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ) : 'Null',
-    React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item one'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item two'
-        )
-    ),
-    React.createElement(
-        'form',
-        null,
-        React.createElement('input', { type: 'text', name: 'option' }),
-        React.createElement(
-            'button',
-            null,
-            'Add option'
-        )
-    )
-);
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+    var option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+    }
+    renderCounterApp();
+    console.log("form submitted");
+};
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+//JSX - Javascript XML extension provided to us by react, not native to javascript
+var renderCounterApp = function renderCounterApp() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title ? app.title + '!' : 'Null'
+        ),
+        app.subtitle ? React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ) : 'Null',
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'Item one'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'Item two'
+            )
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add option'
+            )
+        )
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -4,25 +4,44 @@ console.log('App.js is running!');
 const app = {
     title: 'Indecision app',
     subtitle: 'Put your choices in the hands of a computer',
-    options: ['One', 'Two']
+    options: []
 };
 
-//JSX - Javascript XML extension provided to us by react, not native to javascript
-const template = (
-    <div>
-        <h1>{(app.title) ? app.title + '!' : 'Null'}</h1>
-        {(app.subtitle) ? <p>{app.subtitle}</p> : 'Null'}
-        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-        <form>
-            <input type="text" name="option" />
-                <button>Add option</button>
-        </form>
-    </div>
-);
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+  if (option){
+      app.options.push(option);
+      e.target.elements.option.value = '';
+  }
+  renderCounterApp();
+  console.log("form submitted")
+};
 
 const appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+//JSX - Javascript XML extension provided to us by react, not native to javascript
+const renderCounterApp = () => {
+    const template = (
+        <div>
+            <h1>{(app.title) ? app.title + '!' : 'Null'}</h1>
+            {(app.subtitle) ? <p>{app.subtitle}</p> : 'Null'}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add option</button>
+            </form>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+renderCounterApp();
+
+
+

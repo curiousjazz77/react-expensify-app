e02744ea6b0ab8d0ffac20c9f768fbee15d0c00a
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-08 20:04:42 -0500

    use objects for variable injection

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,9 @@
 
 console.log('App.js is running!');
 
+//Create app object title/subtitle
+
+
 //JSX - Javascript XML extension provided to us by react, not native to javascript
 var template = React.createElement(
     'div',
@@ -15,8 +18,71 @@ var template = React.createElement(
         'p',
         null,
         'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            ' Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            ' Item two'
+        )
     )
 );
+
+var user = {
+    name: 'Idrin',
+    age: 27,
+    location: 'NY'
+
+    //Dynamic injection
+};var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name + '!'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
+);
+
+var titleSubtitle = {
+    title: 'Hello',
+    subtitle: 'my people'
+};
+
+var templateThree = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        titleSubtitle.title + '!'
+    ),
+    React.createElement(
+        'p',
+        null,
+        titleSubtitle.subtitle
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateThree, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,12 +1,48 @@
 console.log('App.js is running!');
 
+//Create app object title/subtitle
+
+
+
 //JSX - Javascript XML extension provided to us by react, not native to javascript
 var template = (
     <div>
         <h1>Indecision App</h1>
         <p>This is some info</p>
+        <ol>
+            <li> Item one</li>
+            <li> Item two</li>
+        </ol>
     </div>
 );
+
+var user = {
+    name: 'Idrin',
+    age: 27,
+    location: 'NY'
+}
+
+//Dynamic injection
+var templateTwo = (
+    <div>
+        <h1>{user.name + '!'}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
+var titleSubtitle = {
+    title: 'Hello',
+    subtitle: 'my people'
+}
+
+var templateThree = (
+    <div>
+        <h1>{titleSubtitle.title+ '!'}</h1>
+        <p>{titleSubtitle.subtitle}</p>
+    </div>
+);
+
 var appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateThree, appRoot);

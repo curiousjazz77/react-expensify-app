916258b0672a6d8012f965cf3c1f9c845c7c520f
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-08 21:16:03 -0500

    solidifying es6 conventions

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,11 @@
+var nameVar = 'Andrew'
+var nameVar = 'Idrin'
+console.log('nameVar', nameVar)
+
+let nameLet = 'Jen';
+nameLet = 'Julie';
+console.log('nameLet', nameLet)
+
+
+const nameConst = 'Juno'
+console.log('nameConst', nameConst)

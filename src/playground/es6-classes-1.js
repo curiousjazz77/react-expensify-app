4cae6c3c39bfd9e81a6f8ced458c7f8721baaaa6
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-09 14:58:22 -0500

    challenge 8

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -11,12 +11,58 @@ class Person {
     }
     getGreeting(){
         // return 'Hi. I am '+ this.name + '!';
-        return `Hi. I am ${this.name} !`;
+        return `Hi. I am ${this.name}!`;
     }
     getDescription(){
         return `${this.name} is ${this.age} year(s) old.`
     }
 }
+
+class Student extends Person{
+    constructor(name, age, major){
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor(){
+        return !!this.major;
+    }
+    getDescription(){
+        let description = super.getDescription();
+        if (this.hasMajor()){
+            description += ` Their major is ${this.major}.`
+        }
+        return description;
+    }
+}
+
+class Traveler extends Person{
+    constructor(name, age, homeLocation){
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    hasHomeLocation(){
+        return !!this.homeLocation;
+    }
+    getGreeting(){
+        let greeting = super.getGreeting();
+        if (this.hasHomeLocation()){
+            greeting += ` I'm visiting from ${this.homeLocation}.`
+        }
+        return greeting;
+    }
+}
+
+const traveler = new Traveler('Idrin ELba', 27, 'Dakar, Senegal');
+console.log(traveler.getGreeting());
+
+const traveler_two = new Traveler('Maritsa Ogunle', 19);
+console.log(traveler_two.getGreeting());
+
+
+const student = new Student('Idrin ELba', 27, 'Computer Science');
+console.log(student.hasMajor());
+console.log(student.getDescription());
+
 
 const me = new Person('Idrin ELba', 27);
 console.log(me);

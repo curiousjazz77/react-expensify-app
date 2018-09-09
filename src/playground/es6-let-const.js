4d867935da481820c399684aea279353e3ccdb4c
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-08 21:21:28 -0500

    more es6

--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -1,11 +1,20 @@
-var nameVar = 'Andrew'
-var nameVar = 'Idrin'
-console.log('nameVar', nameVar)
+//we won't be using var - issues with it
+var nameVar = 'Andrew';
+var nameVar = 'Idrin';
+console.log('nameVar', nameVar);
 
 let nameLet = 'Jen';
 nameLet = 'Julie';
-console.log('nameLet', nameLet)
+console.log('nameLet', nameLet);
 
 
-const nameConst = 'Juno'
-console.log('nameConst', nameConst)
+const nameConst = 'Juno';
+console.log('nameConst', nameConst);
+
+function getPetName(){
+    var petaName = 'Hal'; //scoped to this function
+    return petName;
+}
+
+getPetName();
+console.log(petName);

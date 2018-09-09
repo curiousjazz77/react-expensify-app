480d572d84d7127cd03f9dba721c9ae89a739886
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-08 22:35:03 -0500

    analyzing arrow function conventions

---
+++ src/playground/es6-arrow-functions2.js
@@ -0,0 +1,26 @@
+//arguments object - no longer bound with arrow functions
+
+const add = function (a, b){
+    console.log(arguments)
+    return a + b;
+};
+
+const addArrow = (a, b) =>{
+    //console.log(arguments) //Uncaught ReferenceError: arguments is not defined
+    return a + b;
+};
+
+console.log(add(55, 1));
+console.log(addArrow(20, 1));
+
+
+// this keyword - no longer bound
+const user = {
+    name: 'Idrin',
+    cities: ['Philadelphia', 'Chicago', 'Cincinnati'],
+    printPlacesLives: function (){
+        console.log(this.name)
+        console.log(this.cities)
+    }
+};
+user.printPlacesLives();

995c95d83f979c0e461119d55f1efab66aa7bc95
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-08 22:22:19 -0500

    utilize arrow name split function

--- src/playground/es6-arrow-functions.js
+++ src/playground/es6-arrow-functions.js
@@ -2,9 +2,18 @@ const square = function (x){
   return x * x
 };
 
-const squareArrow = (x) => {
-    return x * x
-    };
+//anonymous function - arrow function
+//like lambdas, more concise functions
+// const squareArrow = (x) => {
+//     return x * x
+//     };
+
+
+const squareArrow = (x) => x * x;
+
+const fullName = 'Idrin Elba';
+const getFirstArrow = (name) => name.split(' ')[0];;
 
 console.log(square(8));
-console.log(squareArrow(9));
+console.log(squareArrow(4));
+console.log(getFirstArrow(fullName));

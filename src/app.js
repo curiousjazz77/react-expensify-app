ae51df3f45658a29122eb2caa242c76ba31840a6
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-09 15:13:01 -0500

    challenge 9

--- src/app.js
+++ src/app.js
@@ -1,16 +1,55 @@
 class Header extends React.Component {
-    render(){
-        return <p>This is from Header</p>;
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        );
     }
 
 }
 
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        )
+    }
+}
+
+//Options component
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                Options component here
+            </div>
+        )
+    }
+}
+
+//AddOption Component
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+               AddOption component here here
+            </div>
+        )
+    }
+}
+
 const jsx = (
     <div>
-        <h1>Title</h1>
-        <Header/>
-        <Header/>
-        <Header/>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
     </div>
 )
 ReactDOM.render(jsx, document.getElementById('app'));

5b7ecb46ceacfc356e084ffc6c7e4498ddd4c3c8
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-11 18:47:06 -0500

    install react via yarn

--- public/bundle.js
+++ public/bundle.js
@@ -709,6 +709,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //install (yarn add validator@8.0.0) ->import ->use
 // we grab the default export from validator library
 //google npm validator
+// yarn add react@16.0.0 react-dom@16.0.0
 
 
 
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 //install (yarn add validator@8.0.0) ->import ->use
 // we grab the default export from validator library
 //google npm validator
+// yarn add react@16.0.0 react-dom@16.0.0
 
 import validator from 'validator';
 

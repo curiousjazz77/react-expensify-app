cdd32a0978b59f8f5e0af3114575358550c31b99
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-11 18:11:10 -0500

    using default import more nuance

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 // import './utils.js';
 
-import subtract, { square, add} from './utils.js'; //proper syntax
+import anythingIWant, { square, add} from './utils.js'; //proper syntax
 import { isAdult, canDrink} from './person.js'; //proper syntax
 
 console.log('app.js is running!');
 console.log(square(4));
 console.log(add(4,5));
-console.log(subtract(4,5));
+console.log(anythingIWant(4,5));
 
 
 

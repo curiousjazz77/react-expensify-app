fc48a239ef3655e367840bf2c9990f9a129f4ad1
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-11 18:38:38 -0500

    import validator to check if is email address

--- src/app.js
+++ src/app.js
@@ -1,20 +1,7 @@
-// import './utils.js';
+//install (yarn add validator@8.0.0) ->import ->use
+// we grab the default export from validator library
+//google npm validator
 
-import anythingIWant, { square, add} from './utils.js'; //proper syntax
-import isSenior, { isAdult, canDrink} from './person.js'; //proper syntax
+import validator from 'validator';
 
-console.log('app.js is running!');
-console.log(square(4));
-console.log(add(4,5));
-console.log(anythingIWant(4,5));
-
-
-
-console.log(isSenior(67));
-
-// console.log(isAdult(12));
-// console.log(isAdult(18));
-// console.log(isAdult(21));
-// console.log(canDrink(12));
-// console.log(canDrink(21));
-// console.log(canDrink(25));
+console.log(validator.isEmail('test'));

430dad22ef33c939355dcb8b04f6da1c2f56df7e
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-11 18:53:06 -0500

    using react in lieu of babel translator

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@
 //google npm validator
 // yarn add react@16.0.0 react-dom@16.0.0
 
-import validator from 'validator';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log(validator.isEmail('test@gmail.com'));
+const template = React.createElement('p', {}, 'testing 123')
+ReactDOM.render(template, document.getElementById('app'));

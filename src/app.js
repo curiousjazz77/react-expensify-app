c193e4b944aac00b24897bf0496c81ee8aa007eb
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-11 21:27:09 -0500

    explore old syntax binding

--- src/app.js
+++ src/app.js
@@ -13,10 +13,16 @@ ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 class OldSyntax {
     constructor(){
         this.name = 'Mike';
+        this.getGreeting = this.getGreeting.bind(this);
+    }
+    getGreeting() {
+        return `Hi. My name is ${this.name}.`;
     }
 }
 const oldSyntax = new OldSyntax();
 console.log(oldSyntax);
+const getGreeting = oldSyntax.getGreeting;
+console.log(getGreeting());
 
 class NewSyntax {
     name = 'Jen'; //done without needing constructor

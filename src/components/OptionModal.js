fe60304dcb2fb547adc31e4a5e2b4eae47d8332a
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-11 22:20:42 -0500

    render option modal to screen 2 but cant close

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Modal from 'react-modal';
 
+//contentLabel is only shown to users with accessibility settings turned on
 const OptionModal = () => (
-        <div>
-            some text
-        </div>
+        <Modal
+        isOpen={true}
+        contentLabel={"Selected Option"}
+        >
+            <h3>Selected Option</h3>
+        </Modal>
 );
 
 export default OptionModal;

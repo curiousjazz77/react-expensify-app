1ad8335254360303e412c9d9aa55609b97eea63d
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-11 21:31:08 -0500

    update addoption to remove constructor and binding

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -7,12 +7,8 @@ export default class AddOption extends React.Component {
         error: undefined
 
     };
-    constructor(props) {
-        super(props);
-        this.handleAddOption = this.handleAddOption.bind(this);
-    }
 
-    handleAddOption(e) { //first handle add option passed in from this component
+    handleAddOption = (e) => { //first handle add option passed in from this component
         e.preventDefault();
         const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);

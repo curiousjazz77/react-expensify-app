bd75d83973751413879b3731a1cbd82bbab6f17d
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-12 01:41:24 -0500

    normalize css

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ import Option from "./Option";
 //sped up because it doesn't have to manage lifecycle
 const Header = (props) => (
     <div className="header">
-        <h1>{props.title}</h1>
-        {props.subtitle && <h2>{props.subtitle}</h2>}
+        <h1 className="header__title">{props.title}</h1>
+        {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
     </div>
 );
 

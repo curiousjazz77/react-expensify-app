ae5d0cc26a411c8a9ebf088375478d71639b31ab
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-12 02:23:55 -0500

    modify buttons to be in final styling

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,7 +3,11 @@ import React from "react";
 
 const Options = (props) => (
     <div>
-        <button onClick={props.handleDeleteOptions}>Remove all</button>
+        <button
+            className="button button--link"
+            onClick={props.handleDeleteOptions}>
+            Remove all
+        </button>
         {props.options.length === 0 && <p>Please add an option to get started!</p>}
         {
             props.options.map((option) => (

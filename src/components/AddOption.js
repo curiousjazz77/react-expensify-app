18400f898abe54a8e2acc086dc732620feec7963
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-11 19:51:56 -0500

    move addoption and option to separate files

---
+++ src/components/AddOption.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+//AddOption Component
+//doesn't make sense for this behavior to live in the parent
+export default class AddOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.state = {
+            error: undefined
+        };
+    }
+
+    handleAddOption(e) { //first handle add option passed in from this component
+        e.preventDefault();
+
+        const option = e.target.elements.option.value.trim();
+        const error = this.props.handleAddOption(option);
+
+        //we will use component state starting here
+        this.setState(() => ({error}));
+
+        if (!error){ //if user doesn't strike an error, clear the input (wipe it)
+            e.target.elements.option.value = '';
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.handleAddOption}>
+                    <input type="text" name="option"/>
+                    <button>Add option</button>
+                </form>
+            </div>
+        )
+    }
+}

9b54898d26b80dcf1a639b0cb5f6feae38d5b884
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-12 01:02:52 -0500

    switch to scss

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
-import  './styles/styles.css';
+import  './styles/styles.scss';
 //here we are in charge of only bootstrapping things that live elsewhere
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));

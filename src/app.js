19b5e9b9fb51096e65ad70c61a45ba76a29601e0
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-12 01:49:43 -0500

    make parts of css variables

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import  'normalize.css/normalize.css';
 import  './styles/styles.scss';
 //here we are in charge of only bootstrapping things that live elsewhere
 

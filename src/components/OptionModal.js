f0034715c403eb8fc469d7fcf1a53774aa3c760e
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-11 22:14:23 -0500

    create object model component

---
+++ src/components/OptionModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const OptionModal = () => {
+    return (
+        <div>
+            some text
+        </div>
+    )
+};
+
+export default OptionModal;

02e00fce7b7c60bb52fb39ff0950ffa152722833
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2018-09-15 03:13:01 -0500

    style modal box with selected option

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,7 +6,9 @@ const OptionModal = (props) => (
     <Modal
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleClearSelectedOption}
-        contentLabel={"Selected Option"}
+        contentLabel="Selected Option"
+        closeTimeoutMS={200}
+        className="modal"
     >
         <h3>Selected Option</h3>
         {props.selectedOption && <p>{props.selectedOption}</p>}
